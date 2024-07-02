8b6ca863375d88d646c69e142891e453bb985069
sagarbendale2004 <sagarbendale2004@gmail.com>
2024-07-02 13:42:07 +0530

    fixes some bugs

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,38 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
+import Button from "../components/Button";
+import Input from "../components/Input";
+import Select from "../components/Select";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import PostForm from "../components/Post-Form/PostForm";
 import RTE from "./RTE";
-
-export { Header, Footer, Container, Logo, LogoutBtn };
+import Signup from "../pages/Signup";
+import Login from "../pages/Login";
+import AddPost from "../pages/AddPost";
+import AllPosts from "../pages/AllPosts";
+import EditPost from "../pages/Editpost";
+import Home from "../pages/Home";
+import Post from "../pages/Post";
+import AuthLayout from "./AuthLayout";
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  RTE,
+  Signup,
+  Login,
+  AddPost,
+  PostForm,
+  AllPosts,
+  EditPost,
+  Home,
+  Post,
+  AuthLayout,
+  Button,
+  Input,
+  Select,
+};
